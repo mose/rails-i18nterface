2dc60f10302e585e8b1975f74644aa724ace8201
Artin Boghosian <artinboghosian@gmail.com>
2011-09-10 18:07:01 -0700

    added rspec set up

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
 source "http://rubygems.org"
 
 gem 'ya2yaml'
+
+group :test do
+  gem 'rspec'
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '04625c26f818af303aa43e3eb869ab9a39b8d879ea91564e560e727e40b93f9bcccd3a2d21bc7f8a16d79cb2c78e5ca1919a91b75a4516180a03e2a1a7c8359a'

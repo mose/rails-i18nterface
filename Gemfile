e8addea8aa3fba013f37f1f7b9cb34810a1e407b
Artin Boghosian <artinboghosian@gmail.com>
2011-09-12 14:17:22 -0700

    got rspec running test

--- Gemfile
+++ Gemfile
@@ -3,5 +3,9 @@ source "http://rubygems.org"
 gem 'ya2yaml'
 
 group :test do
+  gem 'capybara'
+  gem 'rails'
   gem 'rspec'
+  gem 'rspec-rails'
+  gem 'sqlite3'
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+# Configure Rails Envinronment
+ENV["RAILS_ENV"] = "test"
+require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+
+require 'rspec/rails'
+
+ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[File.join(ENGINE_RAILS_ROOT, "spec/support/**/*.rb")].each {|f| require f }
+
+RSpec.configure do |config|
+ config.use_transactional_fixtures = true
+ config.include Capybara::DSL, :example_group => { :file_path => /\bspec\/requests\// }
+ config.include RailsI18nterface::Engine.routes.url_helpers
+end

0e5477dcf2e6fc48cbf42fc484ae121db442e6eb
Michal Hantl <michal.hantl@gmail.com>
2011-09-22 13:54:15 +0200

    export ALL translations, even from old yaml

--- app/controllers/rails_i18nterface/translate_controller.rb
+++ app/controllers/rails_i18nterface/translate_controller.rb
@@ -22,8 +22,8 @@ module RailsI18nterface
 
     def export
       locale = params[:locale].to_sym
-      #keys = {locale => I18n.backend.send(:translations)[locale]}
-      keys = {locale => {}}
+      keys = {locale => I18n.backend.send(:translations)[locale]}
+      #keys = {locale => {}}
       Translation.where(:locale => @to_locale).each { |translation|
         next if translation.value == ''
         next if !translation.value

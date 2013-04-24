4ee83f6b3bd4aa58ec03577b190a2eafb9334c28
mose <mose@mose.com>
2013-04-24 15:02:37 +0800

    testign of the cache lib

--- lib/rails-i18nterface.rb
+++ lib/rails-i18nterface.rb
@@ -1,5 +1,6 @@
 require 'rails-i18nterface/engine'
 require 'rails-i18nterface/utils'
+require 'rails-i18nterface/cache'
 require 'rails-i18nterface/yamlfile'
 require 'rails-i18nterface/sourcefiles'
 require 'rails-i18nterface/keys'
--- lib/rails-i18nterface/cache.rb
+++ lib/rails-i18nterface/cache.rb
@@ -1,21 +1,24 @@
 module RailsI18nterface
   module Cache
 
-    def save_store(obj,uri,options={})
+    def cache_save(obj, uri, options={})
+      FileUtils.rm uri if File.exists? uri
       File.open(uri,'wb') do |f|
         Marshal.dump(obj,f)
       end
     end
 
-    def load_store(uri, &process)
-      if stored? uri
+    def cache_load(uri, file, &process)
+      mtime = File.mtime(file)
+      if cached? uri and uptodate? uri, file
         File.open(uri) do |f|
           Marshal.load f
         end
       else
         if block_given?
           obj = yield
-          save_store(obj, uri)
+          cache_save(obj, uri)
+          File.utime(mtime, mtime, uri)
           obj
         else
           nil
@@ -23,8 +26,12 @@ module RailsI18nterface
       end
     end
 
-    def stored?(uri)
+    def cached?(uri)
       File.file? uri
+    end
+
+    def uptodate?(uri, file)
+      File.mtime(uri) == File.mtime(file)
     end
 
   end

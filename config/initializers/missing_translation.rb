71313cb178271e3b25c9d5673ecffafac88fb5b5
mose <mose@mose.com>
2015-12-24 02:36:03 +0800

    fix missing key patch for when key is a symbol fix #1

--- config/initializers/missing_translation.rb
+++ config/initializers/missing_translation.rb
@@ -4,7 +4,7 @@ module RailsI18nterface
   module UseKeyForMissing
     def call(exception, locale, key, options)
       if exception.is_a?(I18n::MissingTranslation)
-        ("\x00" + key.split(/\./)[-1] + "\x0b").html_safe
+        ("\x00" + key.to_s.split(/\./)[-1] + "\x0b").html_safe
       else
         super
       end
